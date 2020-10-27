4807437c6c1bd6edd1d9c9c1d784f5546abb6123
dimasikjitss <novikov.kgz@gmail.com>
2020-10-27 19:55:41 +0600

    Add project

--- main/settings/productions.py
+++ main/settings/productions.py
@@ -144,7 +144,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS=[(os.path.join(BASE_DIR,'static'))]
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
